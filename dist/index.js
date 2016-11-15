4eea53cb65cb4677caa5e30f18153205955753e4
Kevin Smith <ksmithbaylor@gmail.com>
2016-11-15 09:18:35 -0500

    0.3.0

--- dist/index.js
+++ dist/index.js
@@ -8,31 +8,21 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-require('babel-polyfill');
-
 var _deepEqual = require('deep-equal');
 
 var _deepEqual2 = _interopRequireDefault(_deepEqual);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var noMoreMethodsAfter = function noMoreMethodsAfter(last) {
-  return new Proxy({}, {
-    // Sort of like method_missing in ruby
-    get: function get() {
-      return function () {
-        throw new Error('SagaRunner: no chained method calls allowed after `' + last + '`');
-      };
-    }
-  });
-};
-
 var UNINITIALIZED = Symbol();
+var WRAPPED = Symbol();
 
 var SagaRunner = function () {
-  // things to throw :)
+  // things to reply with
   function SagaRunner(saga) {
     _classCallCheck(this, SagaRunner);
 
@@ -42,20 +32,22 @@ var SagaRunner = function () {
     this.yieldedValues = UNINITIALIZED;
     this.alwaysThrow = UNINITIALIZED;
     this.alwaysReturn = UNINITIALIZED;
+    this.currentState = UNINITIALIZED;
 
     this.assertValidSaga(arguments);
     this.saga = saga;
-  } // things to reply with
+  } // things to throw :)
 
 
   _createClass(SagaRunner, [{
     key: 'expects',
     value: function expects(value) {
       this.assertHasNotRun('cannot call `expects` after `run`');
-      this.expectations.push(value);
+      var expectation = wrapValue(value, this.currentState);
+      this.expectations.push(expectation);
       return {
-        returns: this.returnsForExpectation.bind(this, value),
-        throws: this.throwsForExpectation.bind(this, value)
+        returns: this.returnsForExpectation.bind(this, expectation),
+        throws: this.throwsForExpectation.bind(this, expectation)
       };
     }
   }, {
@@ -70,25 +62,30 @@ var SagaRunner = function () {
     value: function returns(reply) {
       this.assertHasNotRun('cannot call `returns` after `run`');
       this.alwaysReturn = reply;
-      return noMoreMethodsAfter('throws');
+      return noMoreMethodsAfter('returns');
     }
   }, {
     key: 'returnsForExpectation',
-    value: function returnsForExpectation(value, reply) {
+    value: function returnsForExpectation(expectation, reply) {
       this.assertHasNotRun('cannot call `returns` after `run`');
-      this.replies.set(value, reply);
+      this.replies.set(expectation, reply);
       return noMoreMethodsAfter('returns');
     }
   }, {
     key: 'throwsForExpectation',
-    value: function throwsForExpectation(value, reply) {
+    value: function throwsForExpectation(expectation, reply) {
       this.assertHasNotRun('cannot call `throws` after `run`');
-      this.tantrums.set(value, reply);
+      this.tantrums.set(expectation, reply);
       return noMoreMethodsAfter('throws');
     }
   }, {
+    key: 'setState',
+    value: function setState(state) {
+      this.currentState = state;
+    }
+  }, {
     key: 'run',
-    value: function run() {
+    value: function run(t) {
       var _this = this;
 
       this.assertHasNotRun('only allowed to run once');
@@ -97,7 +94,7 @@ var SagaRunner = function () {
       var iterator = this.alwaysThrow === UNINITIALIZED ? this.saga.next() : this.saga.throw(this.alwaysThrow);
 
       var _loop = function _loop() {
-        var yieldedValue = iterator.value;
+        var yieldedValue = wrapValue(iterator.value, UNINITIALIZED);
         _this.yieldedValues.push(yieldedValue);
 
         // Convenience functions for this yieldedValue
@@ -122,6 +119,10 @@ var SagaRunner = function () {
       while (!iterator.done) {
         _loop();
       }
+
+      if (t) {
+        this.runAssertions(t);
+      }
     }
   }, {
     key: 'yielded',
@@ -130,10 +131,21 @@ var SagaRunner = function () {
       return arrayDeepEqualIncludes(this.yieldedValues, value);
     }
   }, {
-    key: 'yieldedAllExpected',
-    value: function yieldedAllExpected() {
+    key: 'runAssertions',
+    value: function runAssertions(t) {
+      var _this2 = this;
+
+      this.assertValidTestObject(t);
       this.assertAlreadyRan('must call `run` before checking yielded values');
-      return this.expectations.every(this.yielded.bind(this));
+      var success = true;
+      this.expectations.forEach(function (expectation) {
+        var yieldedThisOne = _this2.yielded(expectation);
+        t.assert(yieldedThisOne, 'yielded ' + JSON.stringify(expectation));
+        if (!yieldedThisOne) {
+          success = false;
+        }
+      });
+      return success;
     }
 
     //----------------------------------------------------------------------------
@@ -143,33 +155,41 @@ var SagaRunner = function () {
     key: 'assertValidSaga',
     value: function assertValidSaga(args) {
       if (args.length !== 1) {
-        // didn't pass any arguments, or passed too many
-        throw new Error('SagaRunner: must pass exactly one argument (the saga ' + 'instance) to the constructor');
+        error('Must pass exactly one argument (the saga instance) to the constructor');
       }
 
       var saga = args[0];
 
       if (saga && saga.constructor && saga.constructor.name === 'GeneratorFunction') {
-        throw new Error('SagaRunner: make sure to call the saga to get an ' + 'instance, not pass in the generator function itself');
+        error('Make sure to call the saga to get an instance, ' + 'not pass in the generator function itself');
       }
 
       if (!saga || (typeof saga === 'undefined' ? 'undefined' : _typeof(saga)) !== 'object' || !saga.constructor || saga.constructor[Symbol.toStringTag] !== 'GeneratorFunction') {
-        throw new Error('SagaRunner: invalid constructor argument');
+        error('Invalid constructor argument');
       }
     }
   }, {
     key: 'assertAlreadyRan',
     value: function assertAlreadyRan(message) {
       if (this.yieldedValues === UNINITIALIZED) {
-        throw new Error('SagaRunner: ' + message);
+        error(message);
       }
     }
   }, {
     key: 'assertHasNotRun',
     value: function assertHasNotRun(message) {
       if (this.yieldedValues !== UNINITIALIZED) {
-        throw new Error('SagaRunner: ' + message);
+        error(message);
       }
+    }
+  }, {
+    key: 'assertValidTestObject',
+    value: function assertValidTestObject(t) {
+      ['assert', 'deepEqual'].forEach(function (method) {
+        if (!(method in t) || typeof t[method] !== 'function') {
+          error('Test object must have a method called `' + method + '`');
+        }
+      });
     }
   }]);
 
@@ -198,7 +218,7 @@ function mapDeepEqualIterate(ifPresent, ifNotPresent) {
       for (var _iterator = map.keys()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
         var key = _step.value;
 
-        if ((0, _deepEqual2.default)(key, relevantKey)) {
+        if (areEqual(key, relevantKey)) {
           return ifPresent(map, key);
         }
       }
@@ -223,8 +243,49 @@ function mapDeepEqualIterate(ifPresent, ifNotPresent) {
 
 function arrayDeepEqualIncludes(array, value) {
   return array.some(function (member) {
-    return (0, _deepEqual2.default)(member, value);
+    return areEqual(member, value);
   });
+}
+
+function areEqual(candidate, lookingFor) {
+  if (!hasProp(candidate, WRAPPED)) candidate = wrapValue(candidate, {});
+  if (!hasProp(lookingFor, WRAPPED)) lookingFor = wrapValue(lookingFor, {});
+
+  if (hasProp(candidate.value, 'SELECT') && hasProp(lookingFor.value, 'SELECT')) {
+    return selectorIsEqual(candidate, lookingFor);
+  }
+
+  return (0, _deepEqual2.default)(candidate.value, lookingFor.value);
+}
+
+function selectorIsEqual(candidate, lookingFor) {
+  return (0, _deepEqual2.default)(candidate.value.SELECT.selector(lookingFor.state), lookingFor.value.SELECT.selector(lookingFor.state));
+}
+
+function error(message) {
+  throw new Error('SagaRunner: ' + message);
+}
+
+function hasProp(obj, prop) {
+  return obj !== null && (typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object' && prop in obj;
+}
+
+function wrapValue(value, state) {
+  var _ref;
+
+  return _ref = {}, _defineProperty(_ref, WRAPPED, true), _defineProperty(_ref, 'value', value), _defineProperty(_ref, 'state', state), _ref;
+}
+
+function noMoreMethodsAfter(last) {
+  var ret = {};
+  Object.getOwnPropertyNames(SagaRunner.prototype).forEach(function (method) {
+    Object.defineProperty(ret, method, {
+      get: function get() {
+        error('No chained method calls allowed after `' + last + '`');
+      }
+    });
+  });
+  return ret;
 }
 
 exports.default = SagaRunner;
